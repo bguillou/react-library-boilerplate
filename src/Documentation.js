aaafd4fd9e8ba3c43eb2bed613d0b2afb219ef84
bguillou <pixmobiles@gmail.com>
2022-09-08 18:09:55 +0200

    Add default documentation component for documentation

---
+++ src/Documentation.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Documentation() {
+  return (
+    <>
+      <h1>Boilerplate documentation examples</h1>
+      <section>
+        <h2>Components</h2>
+        <section>
+          <h3>Counter</h3>
+        </section>
+      </section>
+    </>
+  );
+}
+
+export default Documentation;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
+import Documentation from "./Documentation";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<div />);
+root.render(<Documentation />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
